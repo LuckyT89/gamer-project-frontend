465befdfa7c311a06dce71be50bc1b39e42f3578
TylerL <tluckewicz@gmail.com>
2021-09-19 15:07:59 -0400

    Add header and style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import GamesPage from './components/GamesPage';
 function App() {
   return (
     <div className="App">
-      My App
+      <div className="header">
+        <h1>Gamer Project</h1>
+      </div>
 
       <Switch>
         <Route exact path="/">
